015e629f4e7c3a4ae9c272e8dfc26cb7597b5ac8
cdansh13 <cdansh13@gmail.com>
2022-07-16 10:52:06 +0530

    Fixed layout and other bugs

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,7 +4,7 @@ export default function Alert(props) {
   return (
     <div style={{height:'50px'}}>
     {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-           {props.alert.msg}
+           {props.alert.msg} 
     </div>
     }
     </div>
