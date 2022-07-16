73c654bd8b180236a1302b083403a04d8948fb3c
cdansh13 <cdansh13@gmail.com>
2022-07-16 10:48:30 +0530

    Fixed layout and other issues

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,31 +5,61 @@ export default function TextForm(props) {
   const [Text,setText] = useState("")
   const handleUpClick = ()=>{
     // console.log("Convert to uppercase")
-    const newText = Text.toUpperCase()
-    props.showAlert("Convert to Uppercase","success")
-    setText(newText)
+    if (Text.length>0){
+      const newText = Text.toUpperCase()
+
+      props.showAlert("Convert to Uppercase","success")
+      setText(newText)
+    }
+    else{
+      props.showAlert("No text","danger")
+    }
   }
   const handleLowClick = ()=>{
     // console.log("Convert to lowercase")
-    const newText = Text.toLowerCase()
-    props.showAlert("Convert to lower Case","success")
-    setText(newText)
+    if (Text.length>0){
+
+      const newText = Text.toLowerCase()
+      props.showAlert("Convert to lower Case","success")
+      setText(newText)
+    }
+    else{
+      props.showAlert("No text","danger")
+    }
   }
   const RemoveExtraSpace = ()=>{
     // console.log("Remove extra space")
-    const newText = Text.split(/[ ]+/)
-    props.showAlert("Remove extra spaces","success")
-    setText(newText.join(" "))
+    if (Text.length>0){
+
+      const newText = Text.split(/[ ]+/)
+      props.showAlert("Remove extra spaces","success")
+      setText(newText.join(" "))
+    }
+    else{
+      props.showAlert("No text","danger")
+    }
   }
   const ClearText = ()=>{
     // console.log("Convert to lowercase")
-    props.showAlert("Clear Text","success")
-    setText("")
+    if  (Text.length>0){
+
+      props.showAlert("Clear Text","success")
+      setText("")
+    }
+    else{
+      props.showAlert("No text","danger")
+    }
   }
   const Copy = ()=>{
     // console.log("Convert to lowercase")
-    props.showAlert("Copy text","success")
-    navigator.clipboard.writeText(Text)
+    if (Text.length>0){
+      
+      props.showAlert("Copy text","success")
+      navigator.clipboard.writeText(Text)
+    }
+    else{
+      props.showAlert("No text","danger")
+    }
   }
   const handleOnChange = (event)=>{
     // console.log("On Change");
@@ -43,11 +73,11 @@ export default function TextForm(props) {
           <h3 htmlFor="exampleInputPassword1" className="form-label">Enter your Text:</h3>
           <textarea type="text" className="form-control" id="name" value={Text} style={{ backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'black'}} onChange={handleOnChange} rows="5"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
-        <button className="btn btn-primary mx-2" onClick={RemoveExtraSpace}>Remove Extra Space</button>
-        <button className="btn btn-primary mx-2" onClick={Copy}>Copy Text</button>
-        <button className="btn btn-primary mx-2" onClick={ClearText}>Clear Text</button>
+        <button className="btn btn-primary mx-1 my-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-1 my-2 " onClick={handleLowClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-1 my-2" onClick={RemoveExtraSpace}>Remove Extra Space</button>
+        <button className="btn btn-primary mx-1 my-2" onClick={Copy}>Copy Text</button>
+        <button className="btn btn-primary mx-1 my-2" onClick={ClearText}>Clear Text</button>
     </div>
     <div className="container  my-2" >
       <h2>Yout text summary</h2>
@@ -55,7 +85,7 @@ export default function TextForm(props) {
       <p>{Text.trim().length } characters</p>
       <p>{Text.length>0?0.008*Text.split(" ").length:"0 " } minutes to read</p>
       <h3>Preview:</h3>
-      <p>{Text.length>0?Text:"Enter some text to preview!"}</p>
+      <p>{Text.length>0?Text:"Nothing to preview!"}</p>
     </div>
     </>
   )
